4a676c225161e09511062bc840f69e9ca88affaf
myplancash <myplancash@gmail.com>
2022-01-04 16:00:03 -0500

    Updates

---
+++ precache-manifest.bdd085eea326983a11929007223aace2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "697781b0f7fda623ca7e",
+    "url": "/clemonsters-rolodex/static/css/main.404af638.chunk.css"
+  },
+  {
+    "revision": "697781b0f7fda623ca7e",
+    "url": "/clemonsters-rolodex/static/js/main.9a40ded1.chunk.js"
+  },
+  {
+    "revision": "72afcd9f6a245884ede2",
+    "url": "/clemonsters-rolodex/static/js/runtime~main.3e23bc17.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/clemonsters-rolodex/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "b00b8e1d09328f33091c",
+    "url": "/clemonsters-rolodex/static/js/3.1cea5788.chunk.js"
+  },
+  {
+    "revision": "8b8e38912f5adc3a0c85a8e147403475",
+    "url": "/clemonsters-rolodex/index.html"
+  }
+];
